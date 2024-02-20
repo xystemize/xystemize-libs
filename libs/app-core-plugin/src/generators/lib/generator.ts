2fc3b77f8801fcacd09dfedb568c25627ac4326f
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-02-20 15:18:01 +0800

    refactor: AppCorePlugin | Update lib generator

--- libs/app-core-plugin/src/generators/lib/generator.ts
+++ libs/app-core-plugin/src/generators/lib/generator.ts
@@ -3,6 +3,7 @@ import {
   generateFiles,
   installPackagesTask,
   names,
+  readJson,
   Tree,
 } from '@nx/devkit';
 import * as path from 'path';
@@ -11,15 +12,21 @@ import { libraryGenerator } from '@nx/js';
 
 export async function libGenerator(tree: Tree, options: LibGeneratorSchema) {
   const { name, directory } = options;
+  const packageJson = readJson(tree, 'package.json');
+  const scopeName = packageJson.scopeName ?? packageJson.name;
+  const libName = names(name).fileName;
   const resolvedOptions = {
     ...options,
-    name: names(name).fileName,
+    name: libName,
+    pascalCaseFiles: true,
+    publishable: true,
+    importPath: `${scopeName}/${libName}`,
   };
 
-  let projectRoot = `libs/${name}`;
+  let projectRoot = `libs/${libName}`;
 
   if (directory) {
-    projectRoot = `libs/${directory}/${name}`;
+    projectRoot = `libs/${directory}/${libName}`;
   }
 
   await libraryGenerator(tree, resolvedOptions);
