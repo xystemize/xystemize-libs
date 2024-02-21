60ad1e55a232f74bf12ebbad8292f9cc890a1f3a
Jeremiah Lachica <jeremiahlachica@outlook.com>
2024-02-21 12:44:01 +0800

    feat: AppCore | Add generator utils

---
+++ libs/app-core/src/Generators/utils/File.ts
@@ -0,0 +1,18 @@
+import {
+    Tree,
+} from '@nx/devkit';
+
+export const readJsonFile = ({ tree, filePath }: { tree: Tree, filePath: string; }) => {
+    return JSON.parse(tree.read(filePath).toString('utf8'));
+};
+
+export const writeJsonFile = ({ tree, filePath, fileContent }: { tree: Tree, filePath: string; fileContent: object }) => {
+    return tree.write(filePath, JSON.stringify(fileContent));
+};
+
+export const appendJsonFile = ({ tree, filePath, fileContent }: { tree: Tree, filePath: string; fileContent: object }) => {
+    const json = readJsonFile({ tree, filePath });
+    Object.assign(json, fileContent)
+
+  writeJsonFile({ tree, filePath, fileContent: json });
+};
